9adf74c9db9fff72a43a7615ead11be72e64141d
Stephane Le Cornec <stephane.lecornec@gmail.com>
2016-04-18 10:29:11 -0400

    9.0 project_issue - copy project tags

---
+++ addons/project_issue/migrations/9.0.1.0/pre-migration.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2014-Today: Odoo Community Association, Microcom
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openupgradelib import openupgrade
+
+
+column_renames = {
+    'project_issue_project_tags_rel': [
+        ('project_category_id', 'project_tags_id'),
+    ],
+}
+
+table_renames = [
+    # rel table named in alphabetical order
+    ('project_category_project_issue_rel', 'project_issue_project_tags_rel'),
+    ]
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.rename_tables(cr, table_renames)
+    openupgrade.rename_columns(cr, column_renames)
