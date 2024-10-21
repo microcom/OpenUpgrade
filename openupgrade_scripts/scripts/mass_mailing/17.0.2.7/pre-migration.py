500f734e5baed16b6759cc0cf73f06d012277e31
Alexis <acp@microcom.ca>
2024-10-21 15:06:01 -0400

    [17.0][OU-ADD] mass_mailing: Migration to 17.0

---
+++ openupgrade_scripts/scripts/mass_mailing/17.0.2.7/pre-migration.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Le Filament (https://le-filament.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_sql_constraint_safely(
+        env,
+        "mass_mailing",
+        "constraint_mailing_contact_list_rel",
+        "unique_contact_list",
+    )
